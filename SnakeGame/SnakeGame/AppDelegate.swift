28c705fcf317cb53cccf9431567d4128cc34ed11
Iboyan Saro <prosi7@icloud.com>
2021-02-03 21:34:06 +0300

    l8
    snake

---
+++ SnakeGame/SnakeGame/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  SnakeGame
+//
+//  Created by Saro Iboyan on 29.01.2021.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+}
+
---
+++ SnakeGame/SnakeGame/SnakeHead.swift
@@ -0,0 +1,23 @@
+//
+//  SnakeHead.swift
+//  SnakeGame
+//
+//  Created by Saro Iboyan on 29.01.2021.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class SnakeHead: SnakeBodyPart {
+
+    override init(atPoint point: CGPoint) {
+        super.init(atPoint: point)
+        self.physicsBody?.categoryBitMask = CollisionCategories.SnakeHead
+        self.physicsBody?.contactTestBitMask = CollisionCategories.EdgeBody | CollisionCategories.Apple | CollisionCategories.Snake
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
